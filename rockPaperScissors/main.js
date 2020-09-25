31e56e9117abd481ff2a1e833d57251f8906864d
ClaireSheek <clairesheek@gmail.com>
2020-09-25 16:27:12 -0500

    passing 2/3 tests

--- rockPaperScissors/main.js
+++ rockPaperScissors/main.js
@@ -16,8 +16,22 @@ const rockPaperScissors = (hand1, hand2) => {
 
   // Write code here
   // Use the unit test to see what is expected
-
-}
+  if (hand1 === hand2){
+    return "It's a tie!"
+  } else if (
+    (hand1 === "rock" && hand2 === "scissors") ||
+    (hand1 === "scissors" && hand2 === "paper") ||
+    (hand1 ==="paper" && hand2 === "rock")) 
+    {
+      return "Hand one wins!"
+    } else (
+      (hand1 === "rock" && hand2 === "paper") ||
+      (hand1 === "scissors" && hand2 === "rock") ||
+      (hand1 ==="paper" && hand2 === "scissors")) 
+      {
+        return "Hand two wins!"
+      }
+    }  
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
