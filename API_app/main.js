96372257fcf0728369d0c19d3fb983fd88b71f37
ClaireSheek <clairesheek@gmail.com>
2020-11-12 22:32:58 -0600

    added styling to api app

--- API_app/main.js
+++ API_app/main.js
@@ -24,6 +24,7 @@ const displayResults = (resultArray) => {
   resultArray.map(drinkResult => {
     const li = document.createElement('li')
     li.id = drinkResult.idDrink
+    li.className = "drinkTitle"
     //create a button to view recipe
     const button = document.createElement('button')
     button.innerHTML = "View Recipe"
@@ -35,33 +36,37 @@ const displayResults = (resultArray) => {
   })
 }
 
+//drops down the drink recipe
 const viewRecipe = (drink) => {
-
-  let drinkName = drink.strDrink
-  let fetchPromise = fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drinkName);
-  let responsePromise = fetchPromise.then(function(response){
-    // console.log(response)
-    return response.json();
-  })
-  responsePromise.then(function(json){
-    json.drinks.forEach(function(drink){
-      console.log("the drink is: ", drink)
-      console.log("the first ingredient is: ", drink.strIngredient1)
-
-  const recipeItem = document.getElementById(drink.idDrink)
-  const recipeBox = document.createElement("div")
-  recipeBox.innerHTML=""
-  console.log(drink)
-  let template = viewRecipeTemplate(drink)
-  console.log(template)
-  recipeBox.innerHTML=template
-  // recipeBox.appendChild(document.createTextNode("Ingredients: " + drink.strIngredient1 + "," + drink.strIngredient2))
-  // recipeBox.appendChild(document.createTextNode("Instructions: " + drink.strInstructions))
-
-  recipeItem.append(recipeBox) 
-  // drink.map(index)
+  if (document.getElementById("currentRecipe")){
+    document.getElementById("currentRecipe").remove()
+  } else{
+    let drinkName = drink.strDrink
+    let fetchPromise = fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drinkName);
+    let responsePromise = fetchPromise.then(function(response){
+      // console.log(response)
+      return response.json();
     })
-  })
+    responsePromise.then(function(json){
+      json.drinks.forEach(function(drink){
+        // console.log("the drink is: ", drink)
+        // console.log("the first ingredient is: ", drink.strIngredient1)
+
+    const recipeItem = document.getElementById(drink.idDrink)
+    const recipeBox = document.createElement("div")
+    recipeBox.id = "currentRecipe"
+    console.log(drink)
+    let template = viewRecipeTemplate(drink)
+    console.log(template)
+    recipeBox.innerHTML=template
+    // recipeBox.appendChild(document.createTextNode("Ingredients: " + drink.strIngredient1 + "," + drink.strIngredient2))
+    // recipeBox.appendChild(document.createTextNode("Instructions: " + drink.strInstructions))
+
+    recipeItem.append(recipeBox) 
+    // drink.map(index)
+      })
+    })
+  }
 }
 
 
@@ -76,18 +81,37 @@ const viewRecipeTemplate = (drink) => {
       continue
     }
     if(key.includes("strIngredient")){
-      ingredientList.push("<li>" + value + "</li>")
+      ingredientList.push("<li class='ingredient'>" + value + "</li>")
+    } 
+  }
+
+  let ingredientMeasurements = []
+  for (const [key, value] of Object.entries(drink)){
+    if(value == null){
+      continue
+    }
+    if(key.includes("strMeasure")){
+      ingredientMeasurements.push("<li class='amount'>" + value + "</li>")
     } 
   }
 
 let ingredientHTML = 
-  `<div>
-    <ul>Ingredients
-       ${ingredientList.join(" ")}
-    </ul>
-    <h3>Instructions</h3>
+  `
+  <div class='ingredientsBox'>
+    <h4>Ingredients</h4>
+    <div class='ingredientsList'>
+      <ul>
+        ${ingredientList.join(" ")}
+      </ul>
+      <ul>
+      ${ingredientMeasurements.join(" ")}
+      </ul>
+    </div>
+ </div>
+ <div class ='instructions'>
+    <h4>Instructions</h4>
     <p> ${drink.strInstructions} </p>
-  </div>
+ </div>
   ` 
   return ingredientHTML
 }
